9ea5aaf45ed8c13cdaac1884e669540b9eac79b0
kevinyu14 <kevinjayyu14@gmail.com>
2019-11-21 16:51:08 -0500

    fixing bugs from transfer cleaning up dictionary import

--- Final_KY/gene_name_fetch.py
+++ Final_KY/gene_name_fetch.py
@@ -1,17 +1,26 @@
+import cobra
+import pandas as pd
+import requests
+
 modelOriginal = cobra.io.load_matlab_model('Recon3D.mat')
+print('model')
 data = pd.read_csv('GSE115469_Data.csv')
+print('data')
 genes_in_sc = data.index
+print('starting json')
 with modelOriginal as model:
     rxns = model.genes
     gene_info = {}
     count = 0
     for i in rxns:
+        print(i)
         url = 'http://bigg.ucsd.edu/api/v2/models/Recon3D/genes/' + str(i)
         r = requests.get(url)
         gene_info[r.json()['name']] = i.id
         count += 1
 f = open('map.txt', 'w')
-f.write(str(gene_info))
+for key,val in gene_info.items():
+    f.write("%s,%s;" % (key, val))
 f.close()
 
 
--- Final_KY/main.py
+++ Final_KY/main.py
@@ -1,5 +1,5 @@
 import cobra
-import matplotlib
+import matplotlib as plt
 import pandas as pd
 import requests
 import scipy.stats
@@ -18,18 +18,22 @@ def optimize_for_gene(name, cell_num, gene_num):
         results[cell_num, gene_num] = FBASolution.objective_value
 
 
-modelOriginal = cobra.io.load_matlab_model('Recon3D.mat')
 data = pd.read_csv('GSE115469_Data.csv')
 genes_in_sc = data.index
-with modelOriginal as model:
-    rxns = model.genes
-    gene_info = {}
-    count = 0
-    for i in rxns:
-        url = 'http://bigg.ucsd.edu/api/v2/models/Recon3D/genes/' + str(i)
-        r = requests.get(url)
-        gene_info[r.json()['name']] = i.id
-        count += 1
+f = open('map.txt', 'r')
+dict_temp = f.readlines()
+list_dict = dict_temp[0].split(';')
+gene_info = {}
+for i in list_dict:
+    if i == '':
+        continue
+    tempkey = i.split(',')[0]
+    tempval = i.split(',')[1]
+    gene_info[tempkey] = tempval
+
+modelOriginal = cobra.io.load_matlab_model('Recon3D.mat')
+
+
 gene_matches = [genes_in_sc[i] for i in range(len(genes_in_sc)) if genes_in_sc[i] in gene_info.keys()]
 
 
@@ -38,7 +42,7 @@ results = np.zeros((num_cells, len(gene_matches[:10])))
 dimnames = []
 for num in range(len(gene_matches[:10])):
     for i in range(len(list(data.loc[gene_matches[0]][:1000]))):
-        print("gene #: %d cell #: %d" %(num, i))
+        print("gene #: %d cell #: %d" % (num, i))
         dimnames.append(gene_matches[num])
         temp_gene_name = gene_matches[num]
         optimize_for_gene(gene_matches(num), i)
@@ -47,11 +51,10 @@ for num in range(len(gene_matches[:10])):
 results_T = results.T
 for i in range(len(results_T)):
     results_T[i] -= np.ones(len(results_T[i])) * scipy.stats.mode(results_T[i])[0]
-matplotlib.rcParams.update({"figure.figsize": (20, 15)})
 fig, axs = plt.subplots(10, 10)
 for i in range(len(results_T)):
     for j in range(len(results_T)):
         axs[i, j].scatter(results_T[i], results_T[j])
         axs[i, j].set_xlabel(dimnames[i * 1000])
         axs[i, j].set_ylabel(dimnames[j * 1000])
-matplotlib.pyplot.tight_layout()
+tight_layout()
