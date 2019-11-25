3abbbb360b8ff908c106a7afe506a91ce5da51b4
kevinyu14 <kevinjayyu14@gmail.com>
2019-11-25 13:46:48 -0500

    add new results and dimensions files

--- Final_KY_Jelly/cluster.py
+++ Final_KY_Jelly/cluster.py
@@ -1,6 +1,24 @@
 import scipy
 import numpy as np
+import matplotlib.pyplot as plt
 
+
+# load the data
 data = np.loadtxt('results.txt.gz')
-dimf = open('dimensions_of_results.txt.gz', 'r')
+# collect the names of genes
+dimf = open('dimensions_of_results.txt', 'r')
 dimnames = dimf.readlines()
+# gene names are separated by ;s
+dimnames = dimnames.split(';')
+# make this so that cell nums are on the rows to cluster cells
+data = data.T
+# take an initial look at the data
+plt.imshow(data, cmap='bwr', aspect='auto')
+plt.show()
+w = scipy.cluster.vq.whiten(data)
+cen, l = scipy.cluster.vq.kmeans2(w, minit='points')
+l = np.array(l)
+l = l[:,np.newaxis]
+tdata= np.concatenate((l, data), 1)
+tdata = tdata[tdata[:, 0].argsort()]
+hlinepos =
